d11b8de223ae61565e20a25a76ff543474e2cd6e
jrober19 <jrober19@umd.edu>
2026-05-13 07:28:19 -0400

    server can now run

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 335-final-project
-Final Project Repo for CMSC335
+Submitted by: Joy Roberts (jrober19)
+Group Members: Joy Roberts (jrober19), Christopher Cannady (jacquicannady)
+App Description:
+YouTube Video Link:
+APIs:
+Contact Email:  [jrober19@terpmail.umd.edu](mailto:jrober19@terpmail.umd.edu)
+Deployed App Link:
+AI Use: 1. Claude, 2. ChatGPT
--- VotingSite.js
+++ VotingSite.js
@@ -5,21 +5,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-require("dotenv").config({
+require("dotenv").config({ //connect environment file
   path: path.resolve(__dirname, "./.env"),
+  quiet: true
 });
 
 const app = express();
 const port = 8000;
 
 app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "templates"));
+app.set("views", path.resolve(__dirname, "templates")); //directory of templates
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); //initialize request.body with post info
+app.use(express.static(__dirname + '/templates')); //to access static CSS file
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Connected to MongoDB with Mongoose");
 
@@ -31,22 +33,35 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
+//home page, fresh load
 app.get("/", (request, response) => {
-  response.render("index");
+  const variables = {
+        img1url: "https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg",
+        img2url: "https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg",
+        img3url: "https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg",
+        label1: "A",
+        label2: "B",
+        label3: "C"
+  };
+  response.render("homepage", variables);
 });
 
+//leaderboard page
 app.get("/leaderboard", (request, response) => {
-  response.render("leaderboard");
+  const variables = {
+        tableEntries: "<tr><td>Col1</td><td>Col2</td><td>Col3</td></tr>"
+  };
+  response.render("leaderboard", variables);
 });
 
 const boardSchema = new mongoose.Schema({
   name: String,
-  id: Number,
-  yesVotes: Number,
-  noVotes: Number
+  id: String,
+  wins: Number,
+  losses: Number
 });
 
-const board = mongoose.model("board", boardSchema);
+const Board = mongoose.model("Board", boardSchema);
 
 
 async function insertApplication(application) {
@@ -61,19 +76,13 @@ async function insertApplication(application) {
 
       const applicant = { name: application.name, email: application.email, gpa: Number(application.gpa), background: application.backgroundInfo };
       let result = await collection.insertOne(applicant);
-    
-   
-
-      
-    
-    
 
 }
 
 
 async function getRandom() {
   try {
-    const randoms = await board.aggregate([
+    const randoms = await Board.aggregate([
       { $sample: { size: 3 } }
     ]);
 
