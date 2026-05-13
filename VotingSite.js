ba8938dfe7e69fb27fe29749c38889b61a52a6e9
= <=>
2026-05-12 21:14:48 -0400

    made it use mongoose

--- VotingSite.js
+++ VotingSite.js
@@ -1,22 +1,35 @@
 "use strict";
 
-const fs = require('fs');
-const ejs = require('ejs');
 const path = require("path");
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
-process.stdin.setEncoding("utf8");
-require("dotenv").config({
-   path: path.resolve(__dirname, "./.env"),
-});
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
 
+require("dotenv").config({
+  path: path.resolve(__dirname, "./.env"),
+});
+
+const app = express();
 const port = 8000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB with Mongoose");
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.get("/", (request, response) => {
   response.render("index");
@@ -25,9 +38,3 @@ app.get("/", (request, response) => {
 app.get("/leaderboard", (request, response) => {
   response.render("leaderboard");
 });
-
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:false}));
-/* Directory where templates will reside */
-app.set("views", path.resolve(__dirname, "templates"));
-
